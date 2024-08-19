13eee04218ef1cfb6ccc23224ebe127d952f0040
atifa_bayan <atifa.bayan@gmail.com>
2024-08-19 16:20:54 +0430

    Add project files.

---
+++ Funtionen/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Funtionen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("It is our FirstDay in Code_Academy");
+            Console.WriteLine("IT is intresting");
+        }
+    }
+}
