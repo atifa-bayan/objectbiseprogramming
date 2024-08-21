ae20c8f3e9ca35a39229292b7ac53028c7b7c113
atifa_bayan <atifa.bayan@gmail.com>
2024-08-21 10:55:11 +0430

    Array and Loop are pushed.

--- Funtionen/Program.cs
+++ Funtionen/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Funtionen
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Funtionen
 {
     internal class Program
     {
@@ -7,6 +9,26 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("It is our FirstDay in Code_Academy");
             Console.WriteLine("IT is intresting");
+            int[] arr = { 1, 2,5 };
+            string[] persons = { "Atifa", "Nadja", "Maren" };
+            int[] arr2 = new int[3];
+            Console.WriteLine(persons[1]);
+            Console.WriteLine(arr.Length);
+            for( int i=0; i<arr.Length; i++)
+            {
+              
+                if (arr[i] == 2)
+                {
+                    return;
+                    
+                }
+                Console.WriteLine(arr[i]);
+            }
+            foreach(string person in persons)
+            {
+                Console.WriteLine(person);
+            }
+
         }
     }
 }
